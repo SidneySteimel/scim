5b7385e9f63da5f15cbd03a15fcfff3f7d0a3e65
Sidney <Sidney@10.20.147.136>
2017-01-16 13:07:37 +0100

    created new class MailManager

---
+++ src/hwr/swe/scim/MailManager.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package hwr.swe.scim;
+
+import java.util.List;
+
+/**
+ * @author Sidney
+ *
+ */
+public class MailManager implements MessageManager {
+
+	/* (non-Javadoc)
+	 * @see hwr.swe.scim.MessageManager#giveMessage(java.util.List)
+	 */
+	@Override
+	public boolean giveMessage(List<String> pList) {
+		// TODO Auto-generated method stub
+		// TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST
+		return false;
+	}
+
+}
