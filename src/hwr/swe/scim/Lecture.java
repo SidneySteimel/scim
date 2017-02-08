af582765377c6e3f1cc388b75f3ebc066e9d0cd1
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2017-02-08 13:48:21 +0100

    Implemented an equlas method for lecture.

--- src/hwr/swe/scim/Lecture.java
+++ src/hwr/swe/scim/Lecture.java
@@ -61,7 +61,34 @@ public class Lecture {
 	public void setIsCreated(boolean pFlag) {
 		this.isCreated = pFlag;
 	}
-	
+
+	/**
+	 * Getter of the name.
+	 * 
+	 * @return the name of the lecture
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Getter of the start time.
+	 * 
+	 * @return the start time of the lecture
+	 */
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * Getetr if the end time.
+	 * 
+	 * @return the end time of the lecture
+	 */
+	public Date getEndTime() {
+		return endTime;
+	}
+
 	/**
 	 * Getter of the isCreated flag.
 	 * 
@@ -85,5 +112,24 @@ public class Lecture {
 		String string = "Veranstaltung: " + name + " am " + startDateString + " von " + startTimeString + " bis "
 				+ endTimeString;
 		return string;
+	}
+
+	/**
+	 * Returns a boolean that says if the lecture is equals with a given object.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o != null && (o instanceof Lecture)) {
+			Lecture secondLecture = (Lecture) o;
+			if (secondLecture.getName().equals(name) && secondLecture.getStartTime().equals(startTime)
+					&& secondLecture.getEndTime().equals(endTime) && secondLecture.getIsCreated() == isCreated) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+
 	}
 }
